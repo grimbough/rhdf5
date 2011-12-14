79f1fef7e441979ee569d62955953fb8c981ee37
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-12-14 00:38:59 +0000

    Removed old package rhdf4 from svn
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rhdf5@61321 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/coerce.R
+++
@@ -1,5 +0,0 @@
-#coerce functions
-
- as.single.hdf5.dataset <- function(x, ...) x
-
- as.double.hdf5.dataset <- function(x, ...) x
--- R/io.R
+++
@@ -1,4 +0,0 @@
-# IO related functions
-
-hdf5.readcel <- function(set, file, create = hdf5.default.properties)
-    .Call("HDF_readCEL", set, file, create, PACKAGE="rhdf5")
