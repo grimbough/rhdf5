c1939f1aef6bf3d632ccf9e4c3bf83144b5e0c32
Bernd Fischer <bernd.fischer@embl.de>
2013-02-26 17:09:46 +0000

    support for reading 64-bit integers added
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rhdf5@73752 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/H5D.R
+++ R/H5D.R
@@ -56,13 +56,24 @@ H5Dget_space <- function( h5dataset ) {
   invisible(h5space)
 }
 
-H5Dread <- function( h5dataset, h5spaceFile=NULL, h5spaceMem=NULL, buf = NULL, compoundAsDataFrame = TRUE) {
+H5Dread <- function( h5dataset, h5spaceFile=NULL, h5spaceMem=NULL, buf = NULL, compoundAsDataFrame = TRUE,
+                     bit64conversion ) {
   h5checktype(h5dataset, "dataset")
   h5checktypeOrNULL(h5spaceFile, "dataspace")
   h5checktypeOrNULL(h5spaceMem, "dataspace")
   if (is.null(h5spaceMem)) { sidMem <- NULL } else { sidMem <- h5spaceMem@ID }
   if (is.null(h5spaceFile)) { sidFile <- NULL } else { sidFile <- h5spaceFile@ID }
-  .Call("_H5Dread", h5dataset@ID, sidFile, sidMem, buf, compoundAsDataFrame, PACKAGE='rhdf5')
+  if (missing(bit64conversion)) {
+    bit64conv = 0L
+  } else {
+    bit64conv = switch(bit64conversion, int = 1L,double = 2L,bit64 = 3L,default=0L)
+  }
+  if (bit64conv == 3L) {
+    if (!require(bit64,quietly=TRUE)) {
+      stop("You have to install the package 'bit64' before you can use the option bit64conversion='bit64'")
+    }
+  }
+  .Call("_H5Dread", h5dataset@ID, sidFile, sidMem, buf, compoundAsDataFrame, bit64conv, PACKAGE='rhdf5')
 }
 
 H5Dwrite <- function( h5dataset, buf, h5spaceMem=NULL, h5spaceFile=NULL ) {
