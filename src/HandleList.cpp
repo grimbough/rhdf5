2a8e03a477adf7d1a7b13328d324f79d7c3e11f2
b.fischer <b.fischer@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-12-11 10:50:08 +0000

    bug fix in returning int value in C++-function removeHandle
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rhdf5@84168 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/HandleList.cpp
+++ src/HandleList.cpp
@@ -47,13 +47,6 @@ SEXP _h5listIdentifier( ) {
   SET_NAMES(Rval, names);
   UNPROTECT(4);
 
-  // SEXP Rval = R_NilValue;
-    // vector<hid_t>::iterator it2;
-    // if (invalidIDs.size() > 0) {
-    //   for (it2 = invalidIDs.begin(); it2 != invalidIDs.end(); it2++) {
-    // 	handleList_.erase(*it2);
-    //   }
-    // }
   return(Rval);
 }
 
@@ -74,137 +67,11 @@ SEXP _h5validObjects( ) {
   return(Rval);
 }
 
-// void
-// addFileHandle(hid_t ID, const char *name) {
-//   //  HandleList::Instance().addHandle(file, ID, name, "/", file, -1, ID);
-// }
 
-// void
-// addGroupHandle(hid_t ID, const char *name, hid_t loc_id) {
-//   /*
-//   H5Handle h;
-//   h = handleInfo(file, loc_id);
-//   char *groupname;
-//   if (h.isvalid != 0) {
-//     groupname = new char[strlen(h.group) + strlen(name) + 1];
-//     strcpy(groupname, h.group);
-//     strcat(groupname, name);
-//     HandleList::Instance().addHandle(group, ID, name, groupname, file, loc_id, loc_id);
-//   } else {
-//     h = handleInfo(group, loc_id);
-//     groupname = new char[strlen(h.group) + strlen(name) + 2];
-//     strcpy(groupname, h.group);
-//     strcat(groupname, "/");
-//     strcat(groupname, name);
-//     HandleList::Instance().addHandle(group, ID, name, groupname, group, h.ID, h.parentFileID);
-//   }
-//   */
-// }
-
-// void
-// addSpaceHandle(hid_t hid, H5S_class_t type) {
-//   /*
-//   char *group;
-//   switch(type) {
-//   case H5S_SCALAR : { group = new char[11]; strcpy(group, "H5S_SCALAR"); } break;
-//   case H5S_SIMPLE : { group = new char[11]; strcpy(group, "H5S_SIMPLE"); } break;
-//   case H5S_NULL : { group = new char[9]; strcpy(group, "H5S_NULL"); } break;
-//   case H5S_NO_CLASS : { group = new char[13]; strcpy(group, "H5S_NO_CLASS"); } break;
-//   default : { group = new char[8]; strcpy(group, "unknown"); } break;
-//   }
-//   HandleList::Instance().addHandle(space, hid, "", group, space, -1, -1);
-//   */
-// }
-
-// void
-// addDatasetHandle(hid_t hid, hid_t loc_id, const char *name, hid_t dtype_id, hid_t space_id) {
-//   /*
-//   if (space_id <= 0) {
-//     space_id = H5Dget_space( hid );
-//     if (space_id > 0) {
-//       H5S_class_t space_type = H5Sget_simple_extent_type(space_id);
-//       addSpaceHandle(space_id, space_type);
-//     }
-//   }
-//   if (dtype_id <= 0) {
-//     dtype_id = H5Dget_type(hid);
-//   }
-//   H5Handle h;
-//   h = handleInfo(file, loc_id);
-//   if (h.isvalid != 0) {
-//     HandleList::Instance().addHandle(dataset, hid, name, "", file, loc_id, loc_id, dtype_id, space_id);
-//   } else {
-//     h = handleInfo(group, loc_id);
-//     HandleList::Instance().addHandle(dataset, hid, name, "", group, h.ID, h.parentFileID, dtype_id, space_id);
-//   }
-//   */
-// }
-
-// void
-// addAttributeHandle(hid_t hid, hid_t obj_id, const char *name, hid_t dtype_id, hid_t space_id) {
-//   /*
-//   if (space_id <= 0) {
-//     space_id = H5Aget_space( hid );
-//     if (space_id > 0) {
-//       H5S_class_t space_type = H5Sget_simple_extent_type(space_id);
-//       addSpaceHandle(space_id, space_type);
-//     }
-//   }
-//   if (dtype_id <= 0) {
-//     dtype_id = H5Aget_type(hid);
-//   }
-//   H5Handle h;
-//   h = handleInfo(file, obj_id);
-//   if (h.isvalid != 0) {
-//     HandleList::Instance().addHandle(attribute, hid, name, "", file, obj_id, obj_id, dtype_id, space_id);
-//   } else {
-//     h = handleInfo(group, obj_id);
-//     if (h.isvalid != 0) {
-//       HandleList::Instance().addHandle(attribute, hid, name, "", group, h.ID, h.parentFileID, dtype_id, space_id);
-//     } else {
-//       h = handleInfo(dataset, obj_id);
-//       HandleList::Instance().addHandle(attribute, hid, name, "", dataset, h.ID, h.parentFileID, dtype_id, space_id);
-//     }
-//   }
-//   */
-// }
-
-int
+void
 removeHandle( hid_t id ) {
-  return (HandleList::Instance().removeHandle( id ) );
+  HandleList::Instance().removeHandle( id );
 }
-
-// int
-// removeHandle( enum HandleType type, hid_t fid ) {
-//   /*
-//   if (type == dataset) {
-//     H5Handle h5h = handleInfo(type, fid);
-//     HandleList::Instance().removeHandle(space, h5h.spaceID);
-//   }
-//   HandleList::Instance().removeHandle(type, fid);
-//   */
-// }
-
-// void 
-// listHandles( ) {
-//   /*
-//   HandleList::Instance().ls();
-//   */
-// }
-
-// SEXP _listHandles() {
-//   /*
-//   listHandles();
-//   */
-//   return R_NilValue;
-// }
-
-// H5Handle handleInfo(enum HandleType type, hid_t ID) {
-//   H5Handle h;
-//   h.isvalid = 0;
-//   // = HandleList::Instance().info(type, ID);
-//   return(h);
-// }
 
 SEXP  handleInfoName( hid_t ID) {
   SEXP Rval = PROTECT(allocVector(VECSXP, 2));
@@ -221,59 +88,6 @@ SEXP  handleInfoName( hid_t ID) {
   return(Rval);
 }
 
-// SEXP  handleInfoDataspace( hid_t ID) {
-//   SEXP Rval = PROTECT(allocVector(VECSXP, 3));
-//   ssize_t st = H5Iget_name( ID, NULL, 0 );
-//   char n1[st+1];
-//   H5Iget_name( ID, (char *)(&n1), st+1 );
-//   SET_VECTOR_ELT(Rval, 0, mkString(n1));
-//   SET_VECTOR_ELT(Rval, 1, mkString(""));
-//   SET_VECTOR_ELT(Rval, 2, mkString(""));
-//   SEXP names = PROTECT(allocVector(STRSXP, 2));
-//   SET_STRING_ELT(names, 0, mkChar("rank"));
-//   SET_STRING_ELT(names, 1, mkChar("dim"));
-//   SET_STRING_ELT(names, 1, mkChar("maxdim"));
-//   SET_NAMES(Rval, names);
-//   UNPROTECT(2);
-//   return(Rval);
-// }
-
-// SEXP  handleInfoDataset( hid_t ID) {
-//   SEXP Rval = PROTECT(allocVector(VECSXP, 2));
-//   ssize_t st = H5Iget_name( ID, NULL, 0 );
-//   char n1[st+1];
-//   H5Iget_name( ID, (char *)(&n1), st+1 );
-//   SET_VECTOR_ELT(Rval, 0, mkString(n1));
-//   SET_VECTOR_ELT(Rval, 1, mkString(""));
-//   SEXP names = PROTECT(allocVector(STRSXP, 2));
-//   SET_STRING_ELT(names, 0, mkChar("name"));
-//   SET_STRING_ELT(names, 1, mkChar("filename"));
-//   SET_NAMES(Rval, names);
-//   UNPROTECT(2);
-//   return(Rval);
-// }
-
-// SEXP  handleInfoAttr( hid_t ID) {
-//   SEXP Rval = PROTECT(allocVector(VECSXP, 2));
-//   ssize_t st = H5Iget_name( ID, NULL, 0 );
-//   char n1[st+1];
-//   H5Iget_name( ID, (char *)(&n1), st+1 );
-//   SET_VECTOR_ELT(Rval, 0, mkString(n1));
-//   st = H5Aget_name( ID, NULL, 0 );
-//   char n1[st+1];
-//   H5Iget_name( ID, (char *)(&n1), st+1 );
-//   SET_VECTOR_ELT(Rval, 1, mkString(n2));
-
-
-//   SET_VECTOR_ELT(Rval, 1, mkString(""));
-//   SEXP names = PROTECT(allocVector(STRSXP, 2));
-//   SET_STRING_ELT(names, 0, mkChar("name"));
-//   SET_STRING_ELT(names, 1, mkChar("filename"));
-//   SET_NAMES(Rval, names);
-//   UNPROTECT(2);
-//   return(Rval);
-// }
-
 SEXP _handleInfo ( SEXP _ID ) {
   int ID = INTEGER(_ID)[0];
   int isvalid = H5Iis_valid(ID);
@@ -283,31 +97,10 @@ SEXP _handleInfo ( SEXP _ID ) {
   H5I_type_t type = H5Iget_type(ID);
   SET_VECTOR_ELT(Rval,1,ScalarInteger(type));
   if (isvalid) {
-    // if ((type != H5I_DATASPACE)) {
-    //   ssize_t st = H5Iget_name( ID, NULL, 0 );
-    //   char n1[st+1];
-    //   H5Iget_name( ID, (char *)(&n1), st+1 );
-    //   SET_VECTOR_ELT(Rval, 2, mkString(n1));
-    // } else {
-    //   SET_VECTOR_ELT(Rval, 2, mkString(""));
-    // }
-    // SET_VECTOR_ELT(Rval, 3, mkString(""));
     switch(type) {
     case H5I_FILE: case H5I_GROUP: case H5I_DATASET: case H5I_ATTR: {
       SET_VECTOR_ELT(Rval, 2, handleInfoName(ID));
     } break;
-    // case H5I_DATATYPE: {
-    //   SET_VECTOR_ELT(Rval, 2, handleInfoDatatype(ID));
-    // } break;
-    // case H5I_DATASPACE: {
-    //   SET_VECTOR_ELT(Rval, 2, handleInfoDataspace(ID));
-    // } break;
-    // case H5I_DATASET: {
-    //   SET_VECTOR_ELT(Rval, 2, handleInfoDataset(ID));
-    // } break;
-    // case H5I_ATTR: {
-    //   SET_VECTOR_ELT(Rval, 2, handleInfoAttr(ID));
-    // } break;
     default: {
       SET_VECTOR_ELT(Rval, 2, R_NilValue);
     }
