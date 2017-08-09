aaf8ca6cc28123320a7b596f7b2770fe7f75cb01
Mike Smith <grimbough@gmail.com>
2017-08-09 12:55:59 +0200

    Fixed bug in h5save() function

--- R/h5save.R
+++ R/h5save.R
@@ -4,16 +4,16 @@ h5save <- function(..., file, name = NULL, createnewfile=TRUE) {
 
   # get object names
   if (is.null(name)) {
-    names <- as.character(substitute(list(...)))[-1L]
+    name <- as.character(substitute(list(...)))[-1L]
   }
-  if (length(names) != N) {
-    stop("length of names must fit the number of objects to save.")
+  if (length(name) != N) {
+    stop("length of 'name' argument  must fit the number of objects to save.")
   }
 
   loc = h5checktypeOrOpenLoc(file, createnewfile=createnewfile)
 
   for (i in 1:N) {
-    h5write(list(...)[[i]], file = loc$H5Identifier, name = names[i])
+    h5write(list(...)[[i]], file = loc$H5Identifier, name = name[i])
   }
 
   h5closeitLoc(loc)
