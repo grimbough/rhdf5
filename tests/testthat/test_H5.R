5cef56e10152469253a692a4f1b8ed4ad3222f2a
Mike Smith <grimbough@gmail.com>
2018-01-12 17:33:35 +0100

    Added tests for h5closeAll()

--- tests/testthat/test_H5.R
+++ tests/testthat/test_H5.R
@@ -12,3 +12,33 @@ test_that("General library functions", {
     expect_is( H5get_libversion(), "integer" ) %>%
         expect_named( c("majnum", "minnum", "relnum") )
 })
+
+############################################################
+context("H5: closing everything")
+############################################################
+
+h5File <- tempfile(pattern = "H5L_", fileext = ".h5")
+if(file.exists(h5File))
+    file.remove(h5File)
+
+test_that("H5closeAll", {
+  
+    ## file
+    fid <- H5Fcreate( h5File )
+    ## dataspace
+    sid <- H5Screate_simple( c(10,5,3) )
+    ## dataset
+    did <- H5Dcreate( fid, name = "dataset", dtype = "H5T_NATIVE_INT8", h5space = sid )
+    ## group
+    gid <- H5Gcreate( fid, name = "group" )
+    ## property list
+    ## attribute
+    ## object
+    
+    expect_equal( nrow(h5listIdentifier()), 4 )
+    
+    expect_silent( h5closeAll() )
+    
+    ## should be nothing left open
+    expect_equal( nrow(h5listIdentifier()), 0 )
+})
---
+++ tests/testthat/test_H5close.R
@@ -0,0 +1,32 @@
+## just some general tests for the moment
+
+# library(rhdf5)
+# tmp_file <- tempfile(fileext = ".h5")
+# h5createFile(tmp_file)
+# h5createGroup(tmp_file,"foo")
+# h5createGroup(tmp_file,"baa")
+# h5createGroup(tmp_file,"foo/foobaa")
+# h5ls(tmp_file)
+# 
+# rhdf5:::h5lsTest("/home/msmith/projects/hdf5_testing/myhdf5file.h5")
+# h5ls("/home/msmith/projects/hdf5_testing/myhdf5file.h5")
+# 
+# L <- .Call("_h5lsTest", H5Fopen("~/h5tutr_dset.h5")@ID, PACKAGE='rhdf5')
+# 
+# H5close()
+# h5ls("/home/msmith/projects/hdf5_testing/myhdf5file.h5")
+# 
+# 
+# 
+# 
+# 
+# fid <- H5Fopen("/home/msmith/projects/hdf5_testing/myhdf5file.h5")
+# gid <- H5Gopen(fid, "/foo")
+# fid2 <- H5Fopen("/home/msmith/projects/hdf5_testing/myhdf5file.h5")
+# oid <- H5Oopen(fid, "/foo")
+# objs <- h5validObjects()
+# 
+# H5Fclose(fid)
+
+
+
