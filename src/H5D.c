c692f6cb9a73e9a0551eedbd040ff6c61ded6fc1
Mike Smith <grimbough@gmail.com>
2019-02-08 17:00:26 +0100

    More ENUM fixes

--- src/H5D.c
+++ src/H5D.c
@@ -877,7 +877,6 @@ SEXP H5Dread_helper(hid_t dataset_id, hid_t file_space_id, hid_t mem_space_id, h
                                        bit64conversion, native );
     } break;
     case H5T_ENUM: {
-        Rprintf("Enum helper\n");
         Rval = H5Dread_helper_ENUM(dataset_id, file_space_id, mem_space_id, n, Rdim, _buf,
                                    dtype_id, cpdType, cpdNField, cpdField, compoundAsDataFrame, native );
     } break;
@@ -973,7 +972,7 @@ SEXP _H5Dread( SEXP _dataset_id, SEXP _file_space_id, SEXP _mem_space_id, SEXP _
     SEXP Rdim;
     int protect_bool = 0;
     
-    if( dtypeclass_id = H5T_ENUM ) {
+    if( dtypeclass_id == H5T_ENUM && rank == 0 ) {
         /* do all ENUM have rank 0? */ 
         Rdim = NULL_USER_OBJECT;
     } else if( (dtypeclass_id == H5T_INTEGER || dtypeclass_id == H5T_FLOAT || dtypeclass_id == H5T_STRING) &&
