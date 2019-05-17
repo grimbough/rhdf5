03e1fe6df43e9b3574c89d5ab8fc9ccd72fd1a16
Mike Smith <grimbough@gmail.com>
2019-05-17 16:52:55 +0200

    Fixed longstanding bug printing source file names when reporting errors.

---
+++ tests/testthat/test_errorReporting.R
@@ -0,0 +1,14 @@
+library(rhdf5)
+
+############################################################
+context("Changing the verbosity of HDF5 error messages")
+############################################################
+
+expect_silent(h5errorHandling('suppress'))
+expect_message(H5Fopen('foo.h5'), "^HDF5")
+
+expect_silent(h5errorHandling('normal'))
+expect_error(H5Fopen('foo.h5'), "no error message captured")
+
+expect_silent(h5errorHandling('verbose'))
+expect_error(H5Fopen('foo.h5'), "libhdf5\n    error #000:")
