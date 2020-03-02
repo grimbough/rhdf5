806e4dd716ec64f16c1ff6ff12bf023b47ac70b7
Mike Smith <grimbough@gmail.com>
2020-03-02 17:57:32 +0100

    Add functionality for setting and getting string padding

--- R/H5T.R
+++ R/H5T.R
@@ -10,7 +10,6 @@ H5Tcopy <- function( dtype_id = h5default(type="H5T")) {
 
 H5Tset_size <- function( dtype_id = h5default(type="H5T"), size) {
   # string constant type_id do not make sense, because they are not allowed to be changed
-  ## dtype_id <- as.integer(dtype_id)
   if (!grepl(pattern = "^[[:digit:]]+$", dtype_id))
       stop("Argument 'dtype_id' should be created by a call to H5Tcopy()")
   size <- as.integer(size)
@@ -20,4 +19,22 @@ H5Tset_size <- function( dtype_id = h5default(type="H5T"), size) {
 ## not currently exported - MLS 23-01-2019
 H5Tget_size <- function( dtype_id = h5default(type="H5T")) {
   .Call("_H5Tget_size", dtype_id, PACKAGE='rhdf5')
+}
+
+
+H5Tset_strpad <- function( dtype_id, strpad = "NULLPAD") {
+  
+  strpad_int <- switch(strpad, 
+                       NULLTERM = 0L, 
+                       NULLPAD = 1L, 
+                       SPACEPAD = 2L, 
+                       stop("Invalid value to 'strpad' argument.\n",
+                            "Valid options are: 'NULLTERM', 'NULLPAD', 'SPACEPAD'"))
+  
+  .Call("_H5Tset_strpad", dtype_id, strpad_int, PACKAGE = "rhdf5")
+}
+  
+ 
+H5Tget_strpad <- function( dtype_id ) {
+  .Call("_H5Tget_strpad", dtype_id, PACKAGE = "rhdf5")
 }
