443aed7441ac5433f523c13a073d05a3f3123671
Mike Smith <grimbough@gmail.com>
2020-03-03 10:43:20 +0100

    Remove unit test that wasn't run.

--- tests/testthat/test_h5read.R
+++ tests/testthat/test_h5read.R
@@ -97,29 +97,6 @@ test_that("Error if asking for something that isn't there", {
 })
 
 
-# test_that("Read / write vector longer than 2^31-1", {
-#     
-#     ## travis doesn't have resources to create an 8GB vector, so we skip there
-#     long_vector <- tryCatch(integer(length = (2^31)+1000),
-#                             error = function(e) NULL)
-#     
-#     if(!is.null(long_vector)) {
-#         expect_silent(
-#             h5createDataset(file = h5File, dataset = "too_long", dims = length(long_vector), 
-#                             level = 0, storage.mode = "integer", chunk = 1e6)
-#         )
-#         expect_silent(
-#             h5write(obj = long_vector, file = h5File, name = "too_long")
-#         )
-#         
-#         rm(long_vector)
-#         
-#         expect_silent( tmp <- h5read(file = h5File, name = "too_long") ) %>%
-#             expect_is("integer") %>%
-#             length() %>%
-#             expect_equal((2^31)+1000)
-#     }
-# })
 
 test_that("writing & reading empty vectors", {
     
