5d5165ea95d42eea0db5d2f1a2eaab6223dbb8a4
Mike Smith <grimbough@gmail.com>
2020-06-05 19:16:14 +0200

    Running S3 tests on when configured with support

--- tests/testthat/test_S3.R
+++ tests/testthat/test_S3.R
@@ -1,22 +1,34 @@
-library(rhdf5)
 
-private_S3_url <- "https://rhdf5-public.s3.eu-central-1.amazonaws.com/h5ex_t_array.h5"
+## only run this test if the S3 VFD is configured in Rhdf5lib
+## Code here looks for the public config header generated when Rhdf5lib was built
+## The functions throw an error if that is not the case and break these tests
+run_test <- FALSE
+HDF5_conf_file <- system.file("include/H5pubconf.h", package  = "Rhdf5lib")
+if(file.exists(HDF5_conf_file)) {
+    run_test <- any(grepl(x = readLines(HDF5_conf_file), "H5_HAVE_ROS3_VFD[[:blank:]]+1"))
+} 
 
-test_that("h5ls() works for files in public S3 buckets", {
-    expect_silent(h5ls_out <- h5ls(private_S3_url, s3 = TRUE))
-    expect_is(h5ls_out, "data.frame")
-    expect_true( "DS1" %in% h5ls_out$name )
-})
+if(run_test) {
 
-test_that("h5dump() works for files in public S3 buckets", {
-    expect_silent(h5dump_out <- h5dump(private_S3_url, s3 = TRUE))
-    expect_is( h5dump_out, "list" )
-    expect_equivalent( length(h5dump_out), 1 )
-    expect_equivalent( dim(h5dump_out$DS1), c(5,3,4) )
-})
+    library(rhdf5)
+    
+    public_S3_url <- "https://rhdf5-public.s3.eu-central-1.amazonaws.com/h5ex_t_array.h5"
+    
+    test_that("h5ls() works for files in public S3 buckets", {
+        expect_silent(h5ls_out <- h5ls(public_S3_url, s3 = TRUE))
+        expect_is(h5ls_out, "data.frame")
+        expect_true( "DS1" %in% h5ls_out$name )
+    })
+    
+    test_that("h5dump() works for files in public S3 buckets", {
+        expect_silent(h5dump_out <- h5dump(public_S3_url, s3 = TRUE))
+        expect_is( h5dump_out, "list" )
+        expect_equivalent( length(h5dump_out), 1 )
+        expect_equivalent( dim(h5dump_out$DS1), c(5,3,4) )
+    })
+    
+    test_that("No open HDF5 objects are left", {
+        expect_equal( length(h5validObjects()), 0 )
+    })
 
-
-test_that("No open HDF5 objects are left", {
-    expect_equal( length(h5validObjects()), 0 )
-})
-
+}
