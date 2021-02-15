20f68e1c6683e7557f21fce86c1397ceb486be2f
Mike Smith <grimbough@gmail.com>
2021-02-15 12:32:03 +0100

    No longer need to redefine HDopen on Windows

--- src/h5testLock.h
+++ src/h5testLock.h
@@ -2,14 +2,4 @@
 #include "myhdf5.h"
 #include <H5private.h>
 
-
-#ifndef HDopen
-  #ifdef H5_HAVE_WIN32_API
-    #define HDopen(S,F,M)       _open(S,F|_O_BINARY,M)
-  #else
-    #define HDopen(F,...)    open(F,__VA_ARGS__)
-  #endif 
-#endif
-
-
 SEXP _h5fileLock();
