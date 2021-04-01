cc087f04cc77b62c95afce1afce1eafd856a9ae5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-04-01 13:48:19 -0700

    Reduced the number of arguments being passed. Added docs.

--- R/h5create.R
+++ R/h5create.R
@@ -203,7 +203,7 @@ h5createDataset <- function(file, dataset, dims, maxdims = dims,
   res
 }
 
-h5createAttribute <- function(obj, attr, dims, maxdims = dims, file, storage.mode = "double", H5type = NULL, size=NULL, cset = c("ASCII", "UTF8"), scalar = FALSE, native = FALSE) {
+h5createAttribute <- function(obj, attr, dims, maxdims = dims, file, storage.mode = "double", H5type = NULL, size = NULL, cset = c("ASCII", "UTF8"), native = FALSE) {
     
     obj = h5checktypeOrOpenObj(obj, file, native = native)
     on.exit(h5closeitObj(obj))
@@ -211,7 +211,7 @@ h5createAttribute <- function(obj, attr, dims, maxdims = dims, file, storage.mod
     res <- FALSE
 
     if (is.numeric(dims) & is.numeric(maxdims)) {
-        if (scalar && length(dims)==1L && length(maxdims)==1L && dims==1L && maxdims==1L) {
+        if (is.null(dims)) {
           sid <- H5Screate()
         } else {
           sid <- H5Screate_simple(dims, maxdims)
@@ -230,7 +230,7 @@ h5createAttribute <- function(obj, attr, dims, maxdims = dims, file, storage.mod
                                       tid <- H5Tcopy("H5T_C_S1")
                                       H5Tset_cset(tid, match.arg(cset))
                                       if (!is.null(size) && !is.numeric(size)) {
-                                        stop("parameter 'size' has to be defined for storage.mode character.")
+                                        stop("'size' should be NULL or a number when 'storage.mode=\"character\"'")
                                       }
                                       H5Tset_size(tid, size) # NULL = variable.
                                       tid
