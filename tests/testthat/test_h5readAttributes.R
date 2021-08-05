a923218be36f6b1f2b595c3a34112c6ea028083b
Mike Smith <grimbough@gmail.com>
2021-08-05 09:53:23 +0100

    Add tests for h5readAttributes

--- tests/testthat/test_h5readAttributes.R
+++ tests/testthat/test_h5readAttributes.R
@@ -6,8 +6,8 @@ if(file.exists(h5File))
     file.remove(h5File)
 
 h5createFile(file = h5File)
-A = 1:10
-h5write(A, h5File, "A")
+A <- as.double(1:10)
+h5write(A, h5File, "A", )
 fid <- H5Fopen(h5File) 
 did <- H5Dopen(fid, "A") 
 sid <- H5Screate_simple(10)
@@ -27,5 +27,21 @@ H5Dclose(did)
 H5Fclose(fid) 
 
 test_that('64-bit integer attributes are read correctly', {
+    
+    expect_warning(x1 <- h5readAttributes(h5File, "A", bit64conversion = "int"))
+    expect_true("int64" %in% names(x1) && "uint32" %in% names(x1))
+    expect_equivalent(x1$int64, c(1:9, NA))
+    expect_equivalent(x1$uint32, c(1:9, NA))
+    
+    expect_silent(x2 <- h5readAttributes(h5File, "A", bit64conversion = "double"))
+    expect_equivalent(x2$int64, c(1:9, 2^32))
+    expect_equivalent(x2$uint32, c(1:9, 2^31))
+    expect_is(x2$int64, "array")
+    expect_type(x2$int64, "double")
+    
+    expect_silent(x3 <- h5readAttributes(h5File, "A", bit64conversion = "bit64"))
+    expect_equivalent(x3$int64, c(1:9, 2^32))
+    expect_equivalent(x3$uint32, c(1:9, 2^31))
+    expect_s3_class(x3$int64, "integer64")
     
 })
