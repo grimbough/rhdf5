caa8bd4697e5b79a227142799c24207c48b530a1
Daniel Smith <dansmith@orst.edu>
2021-11-03 12:03:08 -0700

    Expose per-class methods for h5writeAttribute
    Since these function names have a "." in them, roxygen will add them to NAMESPACE as e.g. `S3method(h5writeAttribute,integer)`. This PR will force oxygen to include them in NAMESPACE as e.g. `export(h5writeAttribute.integer)`, thereby restoring the exports present prior to PR #87.

--- R/h5writeAttr.R
+++ R/h5writeAttr.R
@@ -21,20 +21,26 @@ h5writeAttribute <- function(attr, h5obj, name, cset=c("ASCII", "UTF8"), variabl
   invisible(res)
 }
 
-#' @export
+#' @export h5writeAttribute.matrix
 h5writeAttribute.matrix <- function(...) { h5writeAttribute.array(...) }
-#' @export
+
+#' @export h5writeAttribute.integer
 h5writeAttribute.integer <- function(...) { h5writeAttribute.array(...) }
-#' @export
+
+#' @export h5writeAttribute.double
 h5writeAttribute.double <- function(...) { h5writeAttribute.array(...) }
-#' @export
+
+#' @export h5writeAttribute.logical
 h5writeAttribute.logical <- function(...) { h5writeAttribute.array(...) }
-#' @export
+
+#' @export h5writeAttribute.character
 h5writeAttribute.character <- function(...) { h5writeAttribute.array(...) }
-#' @export
+
+#' @export h5writeAttribute.default
 h5writeAttribute.default <- function(attr, h5obj, name, ...) { warning("No function found to write attribute of class '",class(attr),"'. Attribute '",name,"' is not written to hdf5-file.") }
 
 #' @rdname h5_writeAttribute
+#' @export h5writeAttribute.array
 h5writeAttribute.array <- function(attr, h5obj, name, cset=c("ASCII", "UTF8"), variableLengthString=FALSE, asScalar=FALSE) {
   if (asScalar) {
     if (length(attr) != 1L) {
