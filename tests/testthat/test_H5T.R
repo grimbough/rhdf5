d7e18974c195e33629ff22585060c35a4594ee64
Mike Smith <grimbough@gmail.com>
2021-11-28 18:03:14 +0100

    Some unit tests for H5T functions

--- tests/testthat/test_H5T.R
+++ tests/testthat/test_H5T.R
@@ -1,7 +1,8 @@
 library(rhdf5)
 
+tid <- H5Tcopy("H5T_C_S1")
+
 test_that("String padding can be read and changed", {
-    tid <- H5Tcopy("H5T_C_S1")
     
     expect_silent(tid2 <- H5Tset_strpad(dtype_id = tid,  strpad = "NULLTERM"))
     expect_equal(H5Tget_strpad(tid), 0L)
@@ -11,12 +12,26 @@ test_that("String padding can be read and changed", {
     expect_equal(H5Tget_strpad(tid), 2L)
 })
 
+test_that("String character set can be read and changed", {
+
+  expect_equal(H5Tget_cset(tid), 0L)
+  
+  expect_silent(H5Tset_cset(tid, cset = "UTF-8")) |>
+    expect_gte(0)
+  
+  expect_equal(H5Tget_cset(tid),1L)
+  
+})
+
 test_that("H5T error handling works", {
     expect_error(H5Tget_strpad())
     expect_error(H5Tset_strpad(dtype_id = tid, strpad = "FOOBAA"))
     
     expect_error(H5Tget_size())
     expect_error(H5Tset_size(dtype_id = "H5T_C_S1"))
+    
+    expect_error(H5Tget_cset())
+    expect_error(H5Tget_cset(dtype_id = tid, cset = "FOOBAA"))
 })
 
 
