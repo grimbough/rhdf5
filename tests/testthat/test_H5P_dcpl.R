3b5e029c08a5f51807878e3b8713d28cecaf09dc
Mike Smith <grimbough@gmail.com>
2022-01-19 14:51:39 +0100

    Make szip test specific to windows versions

--- tests/testthat/test_H5P_dcpl.R
+++ tests/testthat/test_H5P_dcpl.R
@@ -6,10 +6,17 @@ test_that("Filters can be set", {
   
   expect_gte(H5Pset_nbit(dcpl), 0)
   expect_gte(H5Pset_shuffle(dcpl), 0)
-  expect_gte(H5Pset_szip(dcpl, options_mask = 1L, pixels_per_block = 8L), 0)
   
   expect_is( H5Pget_nfilters(dcpl), "integer" ) |>
-    expect_equal(3L)
+    expect_equal(2L)
+  
+  ## we can only set szip for writing with Windows versions built after switching to ucrt
+  if( (.Platform$OS.type != "windows") || 
+     (!is.null(R.version$crt) && R.version$crt == "ucrt") ) {
+    expect_gte(H5Pset_szip(dcpl, options_mask = 1L, pixels_per_block = 8L), 0)
+    expect_equal(H5Pget_nfilters(dcpl), 3L)
+  }
+
   
 })
 
