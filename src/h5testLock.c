539aa8a5153d09cd4de6a5cbf22a01897dd76e6b
Hugo Gruson <git@hugogruson.fr>
2026-02-16 14:32:37 +0100

    Do not use private header to handle file locking
    Co-authored-by: LTLA <infinite.monkeys.with.keyboards@gmail.com>

--- src/h5testLock.c
+++ src/h5testLock.c
@@ -3,20 +3,18 @@
 SEXP _h5fileLock(SEXP _file_name) {
   
   const char *file_name = CHAR(STRING_ELT(_file_name, 0));
-  int fd = -1;
-  int lk = -1;
 
   /* create the temporary file */
-  fd = HDopen(file_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
+  H5FD_t* fd = H5FDopen(file_name, H5F_ACC_CREAT, H5P_DEFAULT, /* maxaddr, max 1 MB file? */ 1000000);
 
   /* try to lock file */
-  lk = HDflock(fd, LOCK_EX | LOCK_NB);
-  
+  herr_t lk = H5FDlock(fd, /* rw = */ 1);
+
   /* unlock so we can remove */
-  HDflock(fd, LOCK_UN);
-  
+  H5FDunlock(fd);
+
   /* close */
-  HDclose(fd);
+  H5FDclose(fd);
   
   /* return value of lock attempt */
   return ScalarInteger(lk);
