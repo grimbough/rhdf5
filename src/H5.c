94f933c50faf2f3fc2f3306b33e9b41b7fdda9b0
Hugo Gruson <git@hugogruson.fr>
2026-07-27 17:32:40 +0200

    Avoid redeclaring functions in .c file

--- src/H5.c
+++ src/H5.c
@@ -1,10 +1,5 @@
 #include "H5.h"
 
-SEXP _H5open(void);
-SEXP _H5close(void);
-SEXP _H5garbage_collect(void);
-SEXP _H5get_libversion(void);
-
 /* herr_t H5open(void) */
 SEXP _H5open(void) {
   herr_t herr = H5open();
